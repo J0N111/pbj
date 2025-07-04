ee371f8071e61b94adeb08dc7edb4beea183d2be
J0N111 <77519213+J0N111@users.noreply.github.com>
2025-07-04 15:57:23 -0500

    sizing changes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,45 @@
 const words = document.getElementById('words');
 const logo = document.getElementById('logo');
+const stacked1 = document.getElementById('stacked1');
+const stacked2 = document.getElementById('stacked2');
+const width = window.innerWidth;
 
 window.addEventListener('scroll', () => {
 
-    if (window.scrollY > 0) {
+    if (window.scrollY > 80) {
 
         words.classList.add('hide');
         logo.classList.remove('hide');
 
-        console.log('Page has been scrolled down');
+        // console.log('Page has been scrolled down');
     } else {
         
         words.classList.remove('hide');
         logo.classList.add('hide');
         
-        console.log('Page is at the top');
+        // console.log('Page is at the top');
     }
 });
+
+// window.addEventListener("resize", () => {
+//     const width = window.innerWidth;
+//     console.log(`New size: ${width}`);
+
+//     if (width <= 766) {
+//         stacked1.classList.remove('row');
+//         stacked2.classList.remove('row');
+//     }
+//     else {
+//         stacked1.classList.add('row');        
+//         stacked2.classList.add('row');
+//     }
+// });
+
+//     if (width <= 766) {
+//         stacked1.classList.remove('row');
+//         stacked2.classList.remove('row');
+//     }
+//     else {
+//         stacked1.classList.add('row');        
+//         stacked2.classList.add('row');
+//     }
